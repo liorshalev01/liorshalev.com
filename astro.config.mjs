750ec19fb25ce5cb90868b748595597410cd53bb
Lior Shalev <liorshalev01@users.noreply.github.com>
2026-06-11 16:50:07 +0300

    Open external links in a new tab (#10)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
 // @ts-check
+import { unified } from "@astrojs/markdown-remark";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://liorshalev.com",
   trailingSlash: "always",
   integrations: [sitemap()],
+  markdown: {
+    processor: unified({
+      rehypePlugins: [
+        [
+          rehypeExternalLinks,
+          { target: "_blank", rel: ["noopener", "noreferrer"] },
+        ],
+      ],
+    }),
+  },
   vite: {
     plugins: [tailwindcss()],
   },
